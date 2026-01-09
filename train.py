3ad01d9d232c69852bc8057d94fb3f0989da8c7e
Ananya-Abhilash <iamananya@gmail.com>
2026-01-10 01:06:53 +0530

    Model-Ridge, alpha-1.0, preprocessing-scaler

--- train.py
+++ train.py
@@ -4,7 +4,7 @@ import joblib
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Ridge
 from sklearn.metrics import mean_squared_error, r2_score
 
 # -----------------------------
@@ -42,7 +42,7 @@ X_test = scaler.transform(X_test)
 # -----------------------------
 # Model Training
 # -----------------------------
-model = LinearRegression()
+model = Ridge(alpha=1.0)
 model.fit(X_train, y_train)
 
 # -----------------------------
