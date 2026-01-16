0d063fb17a7d8e17effcad70ee58bbd8f76bd7cb
Ananya-Abhilash <iamananya@gmail.com>
2026-01-16 14:51:26 +0530

    Model-LinearRegression, preprocessing-scalar,test-split 0.2

--- train.py
+++ train.py
@@ -4,7 +4,7 @@ import joblib
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import Lasso
+from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 
 # -----------------------------
@@ -35,14 +35,14 @@ X_train, X_test, y_train, y_test = train_test_split(
 # -----------------------------
 # Preprocessing (Scaling)
 # -----------------------------
-'''scaler = StandardScaler()
+scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
-X_test = scaler.transform(X_test)'''
+X_test = scaler.transform(X_test)
 
 # -----------------------------
 # Model Training
 # -----------------------------
-model = Lasso(alpha=0.1)
+model = LinearRegression(alpha=0.1)
 model.fit(X_train, y_train)
 
 # -----------------------------
